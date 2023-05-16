82279561245280f1673dfc2945a47eb8f2b5c613
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-05-16 19:12:02 +0100

    card image added when hit button is clicked

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,19 @@
+let blackjackGame = {
+    'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+    'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+};
+
+const YOU = blackjackGame['you'];
+const DEALER = blackjackGame['dealer'];
+
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
 
 function blackjackHit() {
-    alert('Ouch!');
+    let cardImage = document.createElement('img');
+    cardImage.src = 'static/images/Q.png';
+    document.querySelector(YOU['div']).appendChild(cardImage);
 }
+
+
 
 
