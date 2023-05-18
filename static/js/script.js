75abb3a11669d1934e522937e9a20c659c4a0ae5
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-05-18 01:37:32 +0100

    card displays is random

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 let blackjackGame = {
     'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
     'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+    'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'Q', 'J', 'A'],
 };
 
 const YOU = blackjackGame['you'];
@@ -13,12 +14,19 @@ document.querySelector('#blackjack-hit-button').addEventListener('click', blackj
 document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
 function blackjackHit() {
-    showCard(YOU);
+    let card = randomCard();
+    console.log(card);
+    showCard(card, YOU);
 }
 
-function showCard(activePlayer) {
+function randomCard() {
+    let randomIndex = Math.floor(Math.random() * 13);
+    return blackjackGame['cards'][randomIndex];
+}
+
+function showCard(card, activePlayer) {
     let cardImage = document.createElement('img');
-    cardImage.src = 'static/images/Q.png';
+    cardImage.src = `static/images/${card}.png`;
     document.querySelector(activePlayer['div']).appendChild(cardImage);
     hitSound.play();
 }
@@ -27,13 +35,12 @@ function blackjackDeal() {
     let yourImages = document.querySelector('#your-box').querySelectorAll('img');
     let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
 
-    yourImages.forEach(function (item) {
-            item.remove();
-        }
-    );
-
-    dealerImages.forEach(function (item) {
-        item.remove();
+    for (i = 0; i < yourImages.length; i++) {
+        yourImages[i].remove();
     }
-);
+
+    for (i = 0; i < dealerImages.length; i++) {
+        dealerImages[i].remove();
+    }
 }
+
