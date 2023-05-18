e640da533a8deec273cd04ef54eaa7fe7247ddbe
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-05-18 01:27:48 +0100

    deal button removes all cards

--- static/js/script.js
+++ static/js/script.js
@@ -10,13 +10,30 @@ const hitSound = new Audio('static/sounds/swish.m4a');
 
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
 
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+
 function blackjackHit() {
+    showCard(YOU);
+}
+
+function showCard(activePlayer) {
     let cardImage = document.createElement('img');
     cardImage.src = 'static/images/Q.png';
-    document.querySelector(YOU['div']).appendChild(cardImage);
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
     hitSound.play();
 }
 
+function blackjackDeal() {
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
 
+    yourImages.forEach(function (item) {
+            item.remove();
+        }
+    );
 
-
+    dealerImages.forEach(function (item) {
+        item.remove();
+    }
+);
+}
